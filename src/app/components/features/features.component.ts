34b192adce7b3fb2f5143fc3be22280f40a59d29
Saichandra12 <saichandra.t123@gmail.com>
2022-04-10 14:37:01 +0530

    Added UI

--- src/app/components/features/features.component.ts
+++ src/app/components/features/features.component.ts
@@ -3,13 +3,41 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-features',
   templateUrl: './features.component.html',
-  styleUrls: ['./features.component.scss']
+  styleUrls: ['./features.component.scss'],
 })
 export class FeaturesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {}
+  ngOnInit(): void {}
+  ar = [];
+  reverseString(str) {
+    return str.split('-').reverse().join('-');
   }
-
+  id = 0;
+  getData(ref) {
+    this.ar.push([
+      ref.value.work,
+      ref.value.desc,
+      this.reverseString(ref.value.dt.split('T')[0]),
+      ref.value.dt.split('T')[1],
+      (this.id = this.id + 1),
+    ]);
+    console.log(ref.value);
+    console.log(this.ar);
+  }
+  arrayRemove(arr, value) {
+    return arr.filter(function (ele) {
+      return ele[4] != value;
+    });
+  }
+  delete(idn) {
+    let i = 0;
+    for (let ind = 0; ind < this.ar.length; ind++) {
+      console.log(this.ar[ind]);
+      if (idn == this.ar[ind][4]) {
+        i = this.ar[ind][4];
+      }
+    }
+    this.ar = this.arrayRemove(this.ar, i);
+    console.log(i);
+  }
 }
